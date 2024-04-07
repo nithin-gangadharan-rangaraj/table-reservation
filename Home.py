6806ddace0cda98184a4f55a1db7a95f179c8402
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-04-07 19:44:41 +1000

    Update Home.py

--- Home.py
+++ Home.py
@@ -15,8 +15,10 @@ st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
 
 def all_time_slots():
     # Define the start and end times
-    start_time = datetime.strptime('11:00', '%H:%M').replace(year=2000, month=1, day=1)
-    end_time = datetime.strptime('22:00', '%H:%M').replace(year=2000, month=1, day=1)
+    start_time = datetime.now().replace(hour = 11, minute = 0)
+    end_time = datetime.now().replace(hour = 22, minute = 0)
+    # start_time = datetime.strptime('11:00', '%H:%M').replace(year=2000, month=1, day=1)
+    # end_time = datetime.strptime('22:00', '%H:%M').replace(year=2000, month=1, day=1)
     
     # Initialize the list to store time slots as datetime objects
     time_slots = []
@@ -29,7 +31,7 @@ def all_time_slots():
     return time_slots
 
 def get_valid_time_slots(time_slots):
-    current_time = datetime.now().replace(year=2000, month=1, day=1)
+    current_time = datetime.now()
     st.write(current_time)
     valid_slots = [f"{t.hour}:{t.minute:02d}" for t in time_slots if t > current_time]
     return valid_slots
