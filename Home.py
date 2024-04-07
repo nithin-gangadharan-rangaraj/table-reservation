256dbab0ecf96ecb79d3e1a3416b187184de7a24
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-04-07 18:05:30 +1000

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from datetime import datetime, timedelta
 
 hide_decoration_bar_style = '''
     <style>
@@ -12,10 +13,20 @@ hide_decoration_bar_style = '''
 '''
 st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
 
+def get_valid_dates():
+
+    today = datetime.now().date()
+    seventh_day = today + timedelta(days=6)
+    return today, seventh_day
+
 def get_info():
   with st.form("reservation", border=False):
     group_size = st.number_input("🤵‍♂️Size of the Group", min_value = 1, max_value = 12)
-    book_date = st.date_input("📅 Reservation Date")
+
+    today, seventh_day = get_valid_dates()
+    book_date = st.date_input("📅 Reservation Date", min_value=today, max_value=seventh_day, format="DD/MM/YYYY")
+
+      
     book_time = st.selectbox("🕛 Pick a Time Slot", options = ['12:00', '12:30'])
     
     reserve_button = st.form_submit_button("Reserve", type = "primary")
