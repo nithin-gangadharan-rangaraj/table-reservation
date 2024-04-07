112243af502cfadb48141784b909c455ef0e564e
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-04-07 16:08:16 +1000

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,4 +1,14 @@
 import streamlit as st
 
+def get_info():
+  with st.form("reservation"):
+    group_size = st.number_input("🤵‍♂️Size of the Group")
+    book_date = st.date_input("📅 Reservation Date")
+    book_time = st.selectbox("🕛 Pick a Time Slot", options = ['12:00', '12:30'])
+    
+    reserve_button = st.form_submit_button("Reserve")
+    if reserve_button:
+      st.write("This functionality is not working yet. :(")
+
 if __name__ == "__main__":
-  st.write("Hello")
+  get_info()
