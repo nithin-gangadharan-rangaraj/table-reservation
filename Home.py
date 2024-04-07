e031dddd6a9a9c52a685f7a9cfa54a5ad1a19a7e
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-04-07 17:33:19 +1000

    Update Home.py

--- Home.py
+++ Home.py
@@ -9,7 +9,7 @@ st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
 
 def get_info():
   with st.form("reservation"):
-    group_size = st.number_input("🤵‍♂️:white[Size of the Group]")
+    group_size = st.number_input("🤵‍♂️Size of the Group")
     book_date = st.date_input("📅 Reservation Date")
     book_time = st.selectbox("🕛 Pick a Time Slot", options = ['12:00', '12:30'])
     
