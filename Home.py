47a92484ece84b4fd7426e77172c87ddf19787bb
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-04-11 13:30:31 +1000

    Update Home.py

--- Home.py
+++ Home.py
@@ -91,6 +91,7 @@ def add_reservation(book_name, book_number, group_size, book_date, book_time, co
             df.loc[len(df) + 1] = {'Name': book_name, 'Group size': group_size, 'Number': book_number, book_time: int(group_size), next_slot: int(group_size)}
         else:
             df.loc[len(df) + 1] = {'Name': book_name, 'Group size': group_size, 'Number': book_number, book_time: int(group_size)}
+        df = df.reset_index(drop=True)
         st.dataframe(df)
         # update_worksheet(conn, book_date.strftime('%d/%m/%Y'), df)
         st.write(f'Reserved! See you at {book_time}')
