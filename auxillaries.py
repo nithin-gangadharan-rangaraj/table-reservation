cb64c36d7a2c816f030af8706e6e1177d7aeeb72
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-04-11 11:55:51 +1000

    Update auxillaries.py

--- auxillaries.py
+++ auxillaries.py
@@ -6,6 +6,7 @@ def initiate():
   conn = st.connection('gsheets', type=GSheetsConnection)
   
   df = conn.read(ttl = 0)
-  worksheets = conn.worksheets
+  spreadsheet = conn.client._open_spreadsheet()  
+  worksheets = spreadsheet.worksheets()
   st.write(worksheets)
   return df
